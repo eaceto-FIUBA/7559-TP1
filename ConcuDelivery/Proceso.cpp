8534c0eb93acbfbd278d36c139a04c22d02da3c4
Kimi <ezequiel.aceto@gmail.com>
2015-04-10 11:44:47 -0300

    SignalHandler en Proceso

--- ConcuDelivery/Proceso.cpp
+++ ConcuDelivery/Proceso.cpp
@@ -4,3 +4,28 @@
 
 #include "Proceso.h"
 
+void Proceso::run() {
+    estadoProceso = CORRIENDO;
+    while (sigint_handler.getGracefulQuit() == 0 && estadoProceso == CORRIENDO) {
+
+    }
+    estadoProceso = PARADO;
+}
+
+Proceso::Proceso() {
+    estadoProceso = INICIALIZADO;
+}
+
+void Proceso::iniciar() {
+    SignalHandler :: getInstance()->registrarHandler ( SIGINT,&sigint_handler );
+    run();
+}
+
+void Proceso::parar() {
+    SignalHandler::getInstance()->removerHandler( SIGINT );
+    estadoProceso = PARADO;
+};
+
+Proceso::~Proceso() {
+
+}
--- ConcuDelivery/Proceso.h
+++ ConcuDelivery/Proceso.h
@@ -12,19 +12,27 @@
 #include <assert.h>
 #include "EventHandler.h"
 #include "SIGINT_Handler.h"
+#include "SignalHandler.h"
 
 class Proceso: public EventHandler{
 
+    enum EstadoProceso {
+        INICIALIZADO,
+        CORRIENDO,
+        PARADO
+    };
+
     private:
+        EstadoProceso estadoProceso;
         SIGINT_Handler sigint_handler;
         virtual void run () = 0;
 
     public:
         Proceso();
-        virtual ~Proceso() { }
+        virtual ~Proceso();
 
-        void stop();
-        void start();
+        void iniciar();
+        void parar();
 };
 
 
